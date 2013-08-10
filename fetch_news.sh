1af5fd60b1ab2b2c7b9ef4f4e06b4d39051ae5bb
borowis <zebrahere@mail.ru>
2013-08-10 19:24:25 +0300

    set oldest articles time to 2 days

--- fetch_news.sh
+++ fetch_news.sh
@@ -7,9 +7,10 @@
 # Name of the recipe to fetch. You can run:
 #     ebook-convert --list-recipes
 # to look for the correct name. Do not forget the .recipe suffix
-declare -a RECIPES=("./recipes/coding_horror.recipe" "./recipes/xkcd.recipe")
-declare -a NAMES=("Coding_Horror" "XKCD")
+declare -a RECIPES=("./recipes/coding_horror.recipe" "./recipes/xkcd.recipe" "./recipes/what_if.recipe")
+declare -a NAMES=("Coding_Horror" "XKCD" "What If")
 OUTDIR="./converted"
+OLDEST_ARTICLES="2"
 
 # Select your output profile. See http://manual.calibre-ebook.com/cli/ebook-convert-14.html
 # for a list. Common choices: kindle, kindle_dx, kindle_fire, kobo, ipad, sony
@@ -23,9 +24,10 @@ DATESTR=`date "+%Y/%m/%d"`
 total=${#RECIPES[*]}
 for (( i=0; i<=$(( $total -1 )); i++ ))
 do
-	OUTFILE="${OUTDIR}/${DATEFILE}_${NAMES[$i]}.mobi"
+	OUTFILE="${OUTDIR}/${DATEFILE}_${NAMES[$i]}.MOBI"
 	echo "Fetching ${RECIPES[$i]} into $OUTFILE"
-	ebook-convert "${RECIPES[$i]}" "$OUTFILE" --output-profile "$OUTPROFILE" --no-inline-toc
+	sed -i "s/oldest_article\( *\)=\( *\)[0-9]\+/oldest_article\1=\2${OLDEST_ARTICLES}/g" ${RECIPES[$i]}
+	ebook-convert "${RECIPES[$i]}" "$OUTFILE" --output-profile "$OUTPROFILE" --no-inline-toc --mobi-keep-original-images
 
 	# Change the author of the ebook from "calibre" to the current date. 
 	# I do this because sending periodicals to a Kindle Touch is removing
@@ -37,5 +39,5 @@ do
 	# a way to keep the peridiocal format when sending through @free.kindle.com 
 	# addresses
 	echo "Setting date $DATESTR as author in $OUTFILE"
-	ebook-meta -a "$DATESTR" "$OUTFILE"
+	ebook-meta "$OUTFILE" -a "$DATESTR"
 done
