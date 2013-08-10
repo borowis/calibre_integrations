13a5f5a73383e024bf665f632899254a024067e3
borowis <zebrahere@mail.ru>
2013-08-10 21:16:25 +0300

    switched to cron completely

--- fetch_news.sh
+++ fetch_news.sh
@@ -7,10 +7,13 @@
 # Name of the recipe to fetch. You can run:
 #     ebook-convert --list-recipes
 # to look for the correct name. Do not forget the .recipe suffix
-declare -a RECIPES=("./recipes/coding_horror.recipe" "./recipes/xkcd.recipe" "./recipes/what_if.recipe")
-declare -a NAMES=("Coding_Horror" "XKCD" "What If")
-OUTDIR="./converted"
-OLDEST_ARTICLES="2"
+FULL_PATH="/home/borowis/Documents/calibre_kindle"
+EBOOK_CONVERT=`which ebook-convert`
+declare -a RECIPES=("${FULL_PATH}/recipes/coding_horror.recipe" "${FULL_PATH}/recipes/xkcd.recipe" "${FULL_PATH}/recipes/what_if.recipe")
+declare -a NAMES=("Coding_Horror" "XKCD" "What If") # postfix feed names and cron jobs keys
+declare -a UPDATE_TIMES=("20" "3" "10") # check for updates interval for every feed; in days; should be less than 28 I think
+OUTDIR="${FULL_PATH}/converted" # conversion output directory
+CRON_TIME_PREFIX="@@min@@ 3 */@@days@@ * *" # time of day to run conversion; see more info on cron; you can change hours but not placeholders
 
 # Select your output profile. See http://manual.calibre-ebook.com/cli/ebook-convert-14.html
 # for a list. Common choices: kindle, kindle_dx, kindle_fire, kobo, ipad, sony
@@ -18,26 +21,24 @@ OUTPROFILE="kindle_dx"
 #
 ######################## End Customization Section #############################
 
-DATEFILE=`date "+%Y_%m_%d"`
-DATESTR=`date "+%Y/%m/%d"`
 
+crontab -l > ${FULL_PATH}/mycron
 total=${#RECIPES[*]}
+cron_time=0 # we need a check so we don't run out of minutes
 for (( i=0; i<=$(( $total -1 )); i++ ))
 do
-	OUTFILE="${OUTDIR}/${DATEFILE}_${NAMES[$i]}.MOBI"
-	echo "Fetching ${RECIPES[$i]} into $OUTFILE"
-	sed -i "s/oldest_article\( *\)=\( *\)[0-9]\+/oldest_article\1=\2${OLDEST_ARTICLES}/g" ${RECIPES[$i]}
-	ebook-convert "${RECIPES[$i]}" "$OUTFILE" --output-profile "$OUTPROFILE" --no-inline-toc --mobi-keep-original-images
+	echo "Set up some variables"
+	sed -i "s/oldest_article\( *\)=\( *\)[0-9]\+/oldest_article\1=\2${UPDATE_TIMES[$i]}/g" ${RECIPES[$i]}
+	CRON_TIME_STRING=`echo "${CRON_TIME_PREFIX}" | sed "s/@@min@@/${cron_time}/g"`
+	CRON_TIME_STRING=`echo "${CRON_TIME_STRING}" | sed "s/@@days@@/${UPDATE_TIMES[$i]}/g"`
+	cron_time=$(( ${cron_time}+5 ))
 
-	# Change the author of the ebook from "calibre" to the current date. 
-	# I do this because sending periodicals to a Kindle Touch is removing
-	# the periodical format and there is no way to differentiate between
-	# two editions in the home screen. This way, the date is shown next 
-	# to the title.
-	# See http://www.amazon.com/forum/kindle/ref=cm_cd_t_rvt_np?_encoding=UTF8&cdForum=Fx1D7SY3BVSESG&cdPage=1&cdThread=Tx1AP36U78ZHQ1I
-	# and, please, email amazon (kindle-feedback@amazon.com) asking to add 
-	# a way to keep the peridiocal format when sending through @free.kindle.com 
-	# addresses
-	echo "Setting date $DATESTR as author in $OUTFILE"
-	ebook-meta "$OUTFILE" -a "$DATESTR"
+	echo "Setting cron job that will fetch ${RECIPES[$i]} into $OUTFILE"
+	sed -i "/^.*${NAMES[$i]}.MOBI.*$/d" ${FULL_PATH}/mycron
+	echo "${CRON_TIME_STRING} ${EBOOK_CONVERT} ${RECIPES[$i]} ${OUTDIR}/`date '+%Y_%m_%d'`_${NAMES[$i]}.MOBI --authors `date '+%Y/%m/%d'` --output-profile $OUTPROFILE --mobi-keep-original-images --no-inline-toc >> ${FULL_PATH}/log" >> ${FULL_PATH}/mycron
 done
+
+# set up new cron
+crontab ${FULL_PATH}/mycron
+rm ${FULL_PATH}/mycron
+
